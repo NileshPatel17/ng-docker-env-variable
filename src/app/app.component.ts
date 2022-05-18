8f17fd4f4219df6fbd130691edb9b02decc9de4d
Nilesh Patel <nilesh.nvs@hotmail.com>
2022-05-18 09:39:53 +0530

    initial commit with cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-docker-env-variable';
+  title = 'Dynamically Set Angular Environment Variables in Docker';
 }
